03eca57dd900d216fc127b91d8f249f553cb6502
miahrashed585 <miahrashed585@gmail.com>
2025-01-02 16:45:58 +0600

    config.py

---
+++ config.py
@@ -0,0 +1,109 @@
+# REQUIRED CONFIG
+BOT_TOKEN = "7857293170:AAF-0kZ4hUONyXo8MQLhLsFrTyr8_xoiQL8"
+OWNER_ID = "7857293170"
+TELEGRAM_API = "25030071"
+TELEGRAM_HASH = "1df165a143dbf49e221f8600b429efa1"
+
+# OPTIONAL CONFIG
+DOWNLOAD_DIR = "/usr/src/app/downloads/"
+DATABASE_URL = ""
+AUTHORIZED_CHATS = ""
+SUDO_USERS = ""
+CMD_SUFFIX = ""                             # Require restart after changing it while bot running
+GDRIVE_ID = "1_iwXBxZTt-RZYi3lIiMsCFoNIWEI46DV"
+IS_TEAM_DRIVE = ""
+STATUS_UPDATE_INTERVAL = ""                       
+AUTO_DELETE_MESSAGE_DURATION = ""
+IGNORE_PENDING_REQUESTS = ""
+USE_SERVICE_ACCOUNTS = ""
+INDEX_URL = ""
+STATUS_LIMIT = ""                                    # Recommended Value : 2 (If Using PICS)
+STOP_DUPLICATE = ""
+UPTOBOX_TOKEN = ""
+TORRENT_TIMEOUT = ""
+EXTENSION_FILTER = ""
+INCOMPLETE_TASK_NOTIFIER = ""
+YT_DLP_QUALITY = ""
+
+# Update
+UPSTREAM_REPO = ""
+UPSTREAM_BRANCH = ""
+
+# Leech
+USER_SESSION_STRING = ""                    # Require restart after changing it while bot running
+LEECH_SPLIT_SIZE = ""
+AS_DOCUMENT = ""
+EQUAL_SPLITS = ""
+LEECH_FILENAME_PERFIX = ""
+MEDIA_GROUP = ""
+DUMP_CHAT = ""
+
+# qBittorrent/Aria2c
+BASE_URL = ""
+SERVER_PORT = ""
+WEB_PINCODE = ""
+
+#Queueing system
+QUEUE_ALL = ""
+QUEUE_DOWNLOAD = ""
+QUEUE_UPLOAD = ""
+
+#Limits
+STORAGE_THRESHOLD = ""
+TORRENT_LIMIT = ""
+DIRECT_LIMIT = ""
+YTDLP_LIMIT = ""
+GDRIVE_LIMIT = ""
+MEGA_LIMIT = ""
+LEECH_LIMIT = ""
+MAX_PLAYLIST = ""
+CLONE_LIMIT = ""
+
+#Group Features
+FSUB_IDS = "-"
+USER_MAX_TASKS = ""
+ENABLE_RATE_LIMIT = ""
+ENABLE_MESSAGE_FILTER = ""
+STOP_DUPLICATE_TASKS = ""
+DISABLE_DRIVE_LINK = ""
+
+#Extra
+SET_COMMANDS = "True"
+DISABLE_LEECH = ""
+DM_MODE = "all"
+DELETE_LINKS = ""
+LOG_CHAT = ""
+
+# RSS
+RSS_DELAY = ""                              # Require restart after changing it while bot running
+RSS_COMMAND = ""
+RSS_CHAT_ID = ""
+RSS_USER_SESSION_STRING = ""                # Require restart after changing it while bot running
+
+# Mega
+MEGA_API_KEY = ""
+MEGA_EMAIL_ID = ""
+MEGA_PASSWORD = ""
+
+# Buttons
+VIEW_LINK = ""
+
+# Torrent Search
+SEARCH_API_LINK = ""
+SEARCH_LIMIT = ""
+SEARCH_PLUGINS = '["https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/rarbg.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/piratebay.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/legittorrents.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/limetorrents.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentscsv.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/zooqle.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/eztv.py",
+                   "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/kickass_torrent.py",
+                   "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/yts_am.py",
+                   "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/linuxtracker.py",
+                   "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/nyaasi.py",
+                   "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/ettv.py",
+                   "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/glotorrents.py",
+                   "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/thepiratebay.py",
+                   "https://raw.githubusercontent.com/nindogo/qbtSearchScripts/master/magnetdl.py",
+                   "https://raw.githubusercontent.com/khensolomon/leyts/master/yts.py"]'
